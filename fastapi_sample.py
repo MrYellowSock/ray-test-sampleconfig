4dba2563a6b16c346f5bee10a2817a2b18108aef
MrYellowSock <44232964+MrYellowSock@users.noreply.github.com>
2024-07-30 16:14:45 +0700

    Add files via upload

---
+++ fastapi_sample.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from typing import Dict
+from ray import serve
+import ray
+import flyingcat
+
+api = FastAPI()
+@serve.deployment
+@serve.ingress(api)
+class MyModelDeployment:
+    def __init__(self):
+        self._msg = "WHY"
+
+    @api.get("/")
+    def root(self) -> Dict:
+        import os
+        import pandas as pd
+        return {"result": self._msg, "hostname": os.uname()[1], "pd_version":pd.__version__}
+
+    @api.post("/ping")
+    def ping(self,name:str) -> Dict:
+        return {"result": f"pong {name}"}
+
+app = MyModelDeployment.bind()
